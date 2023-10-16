693ff5e6f5f7ef4e797852f0c4ef62ce40524eae
MatteoArt <mattrim8@gmail.com>
2023-10-16 22:34:42 +0200

    fine milestone 5

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,37 @@
 <script>
 
-   import axios from 'axios';
+import axios from 'axios';
+import ProjectCard from './components/ProjectCard.vue';
 
 
-    export default {
-      data() {
-        return {
-          projects: []
-        }
-      },
-      methods: {
-        //metodo che ha il compito di recuperare i dati dall'API tramite axios
-        fetchData() {
-          axios.get('http://127.0.0.1:8000/api/projects')
-          .then((response) => {
-            //ottengo i dati relativi ai progetti
-            let ris = response.data.results;
-
-            console.log(ris);
-          })
-        }
-      },
-      mounted() {
-        this.fetchData();
-      }
+export default {
+  components: {
+    ProjectCard
+  },
+  data() {
+    return {
+      projects: []
     }
+  },
+  methods: {
+    //metodo che ha il compito di recuperare i dati dall'API tramite axios
+    fetchData() {
+      axios.get('http://127.0.0.1:8000/api/projects')
+        .then((response) => {
+          //ottengo i dati relativi ai progetti
+          let ris = response.data.results;
+
+          //salvo i dati nella variabile projects
+          this.projects = ris;
+
+          console.log(this.projects);
+        })
+    }
+  },
+  mounted() {
+    this.fetchData();
+  }
+}
 </script>
 
 <template>
@@ -34,9 +41,12 @@
     <h2>Welcome, guest</h2>
   </div>
   <h2 class="mt-3 text-center text-info-emphasis">Projects</h2>
-
+  <div class="container-md mt-2 mb-4">
+    <div class="row gy-3">
+      <ProjectCard v-for="project in projects"
+      :projectData="project" :key="project.id"></ProjectCard>
+    </div>
+  </div>
 </template>
 
-<style>
-
-</style>
+<style></style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,52 @@
+<script>
+
+export default {
+    props: {
+        projectData: {
+            type: Object,
+            required: true,
+        }
+    },
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        getImagePath(image) {
+            return `http://127.0.0.1:8000/storage/${image}`;
+        }
+    }
+}
+
+</script>
+
+<template>
+    <div class="col-4">
+        <div class="card h-100">
+            <img :src="getImagePath(projectData.img)" class="card-img-top" alt="img-project">
+            <div class="card-body">
+                <h3 class="card-title">{{ projectData.title }}</h3>
+                <span class="badge text-bg-warning">
+                    {{ projectData.type.name }}
+                </span>
+                <p class="card-text mt-2">
+                    {{ projectData.description }}
+                </p>
+                <div class="mt-2 mb-3">
+                    <div>Linguaggi/tecnologie:</div>
+                    <span class="badge text-bg-info me-2" 
+                    v-for="technology in projectData.technologies">
+                            {{ technology.name }}
+                    </span>
+                </div>
+                <a :href="projectData.repository" class="btn btn-outline-success" target="_blank">Repository</a>
+                <a v-if="projectData.page_project" 
+                :href="projectData.page_project" 
+                class="btn btn-outline-info ms-3" target="_blank">Pagina del progetto</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>

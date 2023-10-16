9aac73ebd965ca905c6339d68d665f6380891dcd
MatteoArt <mattrim8@gmail.com>
2023-10-16 18:40:06 +0200

    milestone 3 e 4

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,42 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+
+   import axios from 'axios';
+
+
+    export default {
+      data() {
+        return {
+          projects: []
+        }
+      },
+      methods: {
+        //metodo che ha il compito di recuperare i dati dall'API tramite axios
+        fetchData() {
+          axios.get('http://127.0.0.1:8000/api/projects')
+          .then((response) => {
+            //ottengo i dati relativi ai progetti
+            let ris = response.data.results;
+
+            console.log(ris);
+          })
+        }
+      },
+      mounted() {
+        this.fetchData();
+      }
+    }
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <h1 class="text-start mt-2 text-success-emphasis">developerFolio</h1>
+
+  <div class="container mt-4 mb-2">
+    <h2>Welcome, guest</h2>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h2 class="mt-3 text-center text-info-emphasis">Projects</h2>
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style>
+
 </style>

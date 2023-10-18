d17438b5e065e236bcbcd921b01a722881b94cb7
MatteoArt <mattrim8@gmail.com>
2023-10-18 11:16:25 +0200

    correzione bug undefined nello show

--- src/pages/SingleProject.vue
+++ src/pages/SingleProject.vue
@@ -15,6 +15,9 @@ export default {
                     this.singleProject = response.data.result[0];
                     console.log(this.singleProject)
                 })
+        },
+        getImagePath(image) {
+            return `http://127.0.0.1:8000/storage/${image}`;
         }
     },
     mounted() {
@@ -26,11 +29,16 @@ export default {
 
 <template>
     <h1 class="text-center mt-3">{{ singleProject.title }}</h1>
-    <span class="badge text-bg-warning">{{ singleProject.type.name }}</span>
     <div class="w-50 m-auto">
+        <span v-if="singleProject.type" class="badge text-bg-warning">
+            {{ singleProject.type.name }}
+        </span>
         <p>
             {{ singleProject.description }}
         </p>
+        <div class="w-50" v-if="singleProject.img">
+            <img class="img-thumbnail" :src="getImagePath(singleProject.img)" alt="img-project">
+        </div>
     </div>
 </template>
 
